f4ed67f911436d07c2ebc910d6dcb3112ec1d3b5
UKX0502 <deepa.balasubramanian@infotel.com>
2025-07-31 19:23:37 +0530

    Zone.js Vs signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,9 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  getImagePath(): string {
-    return 'assets/users/' + this.selectedUser.avatar;
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // getImagePath(): string {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
