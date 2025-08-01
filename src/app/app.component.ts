b7eac0eed30f94a98880f7a5df7b8055c2976179
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-01 13:46:02 +0530

    use of if else in app html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { UserComponent } from "./user/user.component";
 
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
