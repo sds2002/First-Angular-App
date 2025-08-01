2e6479e17d9059549d412640b93ad21477ceca6d
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-01 12:20:16 +0530

    output signal code reverted

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -14,8 +14,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  // @Output() select = new EventEmitter;
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
