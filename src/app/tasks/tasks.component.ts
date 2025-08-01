d3290ff5be4478ff5dde40f07477aacf99c379ca
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-01 15:54:45 +0530

    new-task component added

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { dummyTasks } from '../dummy-tasks';
+import { NewTask } from './new-task/new-task';
 import { Task } from './task/task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input() name!: string;
   @Input({required: true}) userId!: String;
+  isAddingTask:boolean =false;
 
   tasks =dummyTasks;
   get selectedUserTasks() {
@@ -19,5 +21,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  OnStartAddTask() {
+    this.isAddingTask = true;
   }
 }
