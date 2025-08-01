04031c4e857d5cef8b3f3802717f5152d192df28
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-01 17:06:34 +0530

    ngMode 2 way data binding

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,13 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [ FormsModule ], //needed for ngModel
   templateUrl: './new-task.html',
   styleUrl: './new-task.css'
 })
 export class NewTask {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+  
   onCancel() {
       this.cancel.emit();
   }
