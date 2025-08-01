ff9ecbd90d9c2a1465ee03dcc97a149c2bd4545f
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-01 13:17:22 +0530

    creating tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserComponent, HeaderComponent],
+  imports: [UserComponent, HeaderComponent,TasksComponent ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
