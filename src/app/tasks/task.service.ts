6ed85da0e510f1578a5049637e33f56587647177
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-01 21:00:04 +0530

    localStorage access and JSON

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -29,6 +29,13 @@ export class TaskService {
   },
 ]
 
+constructor() {
+  const tasks = localStorage.getItem('tasks');
+
+  if (tasks) {
+    this.tasks = JSON.parse(tasks);
+  }
+}
 getUserTasks(userId: string ) {
   return this.tasks.filter((task) => task.userId === userId);
 }
@@ -41,10 +48,15 @@ addTask(taskData: NewTaskData, userId: string) {
     summary: taskData.summary,
     dueDate: taskData.duedate
   });
+  this.saveTasks();
 }
 
 removeTask(id: string) {
   this.tasks = this.tasks.filter((task) => task.id !== id);
+  this.saveTasks();
 }
 
+private saveTasks() {
+  localStorage.setItem('tasks',JSON.stringify(this.tasks));
+}
 }
