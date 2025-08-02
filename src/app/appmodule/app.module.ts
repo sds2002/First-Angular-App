82e18d640ccc074b46b5f8c325c74aaa11a6f6bb
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-02 07:42:56 +0530

    added shared module

--- src/app/appmodule/app.module.ts
+++ src/app/appmodule/app.module.ts
@@ -5,15 +5,15 @@ import { TasksComponent } from '../tasks/tasks.component';
 import { UserComponent } from '../user/user.component';
 import { BrowserModule } from '@angular/platform-browser'
 import { NewTask } from '../tasks/new-task/new-task.component';
-import { CardComponent } from '../shared/card/card.component';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../tasks/task/task.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, Task, NewTask, CardComponent],
+  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, Task, NewTask],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule]
+  imports: [BrowserModule, FormsModule, SharedModule]
 })
 export class AppModule { }
