b304abf282cdb63558eab403384e1cbbdb0ed4eb
UKX0502 <deepa.balasubramanian@infotel.com>
2025-08-02 07:11:13 +0530

    added app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from './dummy-users';
-import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [UserComponent, HeaderComponent,TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
